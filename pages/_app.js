815d22af5cd87cd02a1cd1acdd9357b1bbac665b
Marsden Jacques <marsdensgardens@gmail.com>
2021-07-25 19:22:20 +0900

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+    <div>
+      <Head>
+        <title>Marsden Jacques</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </div>)
 }
 
 export default MyApp
